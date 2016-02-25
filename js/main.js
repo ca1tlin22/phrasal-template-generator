72d502255b3cd20c1394534f535d570efe8224b4
ca1tlin22 <gagn0372@algonquinlive.com>
2016-02-25 12:06:53 -0500

    content/text added

--- js/main.js
+++ js/main.js
@@ -4,6 +4,20 @@ var colour = prompt('Choose a colour (orange, indigo, etc.).');
 
 var creature = prompt('Choose a creature (dragon, moth, etc.).');
 
-var adjective = prompt('Choose an adjective (beautiful, super, etc.).')
+var adjective = prompt('Choose an adjective (beautiful, super, etc.).');
 
-var verb = prompt('Choose a past tense verb (ran, burped, etc.).');
+var verbed = prompt('Choose a past tense verb (ran, burped, etc.).');
+
+var story = prompt('Which story would you like? a) Snacking b) Escaping c) Winning Enter the letter below:');
+
+if(story == a) {
+  alert('After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.');
+}
+
+if(story == b) {
+  alert('Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.');
+}
+
+if(story == c) {
+  alert('Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.');
+}
