0783a5b43fc0020be1581977f93cbafa1432aa8f
ca1tlin22 <gagn0372@algonquinlive.com>
2016-02-25 14:35:15 -0500

    edited alerts for stories

--- js/main.js
+++ js/main.js
@@ -11,13 +11,13 @@ var verbed = prompt('Choose a past tense verb (ran, burped, etc.).');
 var story = prompt('Which story would you like? a) Snacking b) Escaping c) Winning Enter the letter below:');
 
 if(story == a) {
-  alert('After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.');
+  alert('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature + verbed + 'for hours.');
 }
 
 if(story == b) {
-  alert('Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.');
+  alert('Ameilia' + verbed + 'through the' + adjective + colour + 'nebula escaping the space' + creature + '.');
 }
 
 if(story == c) {
-  alert('Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.');
+  alert('Jackson chose his' + adjective + ',' + colour + creature + 'card and' + verbed + 'it to the table knowing he won.');
 }
