b20411fbf41db49fb29d45b3f7247b3fe86b435e
ca1tlin22 <gagn0372@algonquinlive.com>
2016-02-25 11:41:37 -0500

    Name changed, js hooked up to index, prompts added to js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+alert('Lets make a story together!');
+
+var colour = prompt('Choose a colour (orange, indigo, etc.).');
+
+var creature = prompt('Choose a creature (dragon, moth, etc.).');
+
+var adjective = prompt('Choose an adjective (beautiful, super, etc.).')
+
+var verb = prompt('Choose a past tense verb (ran, burped, etc.).');
