1d3ba43e7ecc5b7015ae76184f52f8124eb50d7f
ca1tlin22 <gagn0372@algonquinlive.com>
2016-02-25 21:37:16 -0500

    figured out issue... which story prompt incorrect

--- js/main.js
+++ js/main.js
@@ -8,15 +8,7 @@ var adjective = prompt('Choose an adjective (beautiful, super, etc.).');
 
 var verbed = prompt('Choose a past tense verb (ran, burped, etc.).');
 
-var story = prompt('Which story would you like?') + var options = ['a) Snacking', 'b) Escaping', 'c) Winning'] + 'Enter the letter below:');
-
-if (story == 'a' || story == 'A') {
-  a();
-} else if (story == 'b' || story == 'B') {
-  b();
-} else if (story == 'c' || story == 'C') {
-  c();
-}
+var story = prompt('Which story would you like?');
 
 var a = function () {
   document.write('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature + verbed + 'for hours.');
@@ -29,3 +21,13 @@ var b = function () {
 var c = function ()  {
   document.write('Jackson chose his' + adjective + ',' + colour + creature + 'card and' + verbed + 'it to the table knowing he won.');
 }
+
+if (story == 'a' || story == 'A') {
+  a();
+} else if (story == 'b' || story == 'B') {
+  b();
+} else if (story == 'c' || story == 'C') {
+  c();
+}
+
+) + var options = ['a) Snacking', 'b) Escaping', 'c) Winning'] + 'Enter the letter below:'
